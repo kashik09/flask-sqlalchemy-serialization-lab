fe599cdcd4ed81b871912ee3a1a038e40621a2d8
kashik09 <kashiku789@gmail.com>
2026-04-22 15:45:35 +0300

    implement review relationships and serialization schemas

--- server/models.py
+++ server/models.py
@@ -16,6 +16,8 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
@@ -27,6 +29,42 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+    reviews = db.relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+
+class CustomerSchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+    reviews = fields.Nested('ReviewSchema', many=True, exclude=('customer',))
+
+
+class ItemSchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+    price = fields.Float()
+    reviews = fields.Nested('ReviewSchema', many=True, exclude=('item',))
+
+
+class ReviewSchema(Schema):
+    id = fields.Integer()
+    comment = fields.String()
+    customer = fields.Nested('CustomerSchema', exclude=('reviews',))
+    item = fields.Nested('ItemSchema', exclude=('reviews',))
